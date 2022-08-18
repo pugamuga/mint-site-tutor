01da35c9f15a9513ccb67fb8073baaca96709fe6
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-08-18 20:19:02 +0300

    add on gh-pages

--- src/components/MainHeaderName.js
+++ src/components/MainHeaderName.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const MainHeaderName = () => {
   return (
     <div>
-        <h1 className='text-white text-3xl text-center -mb-12 mt-20 drop-shadow-[4px_4px_0px_black]'>Manki NFT</h1>
+      <h1 className="text-white text-3xl text-center -mb-12 mt-20 drop-shadow-[4px_4px_0px_black]">
+        Manki NFT
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default MainHeaderName
+export default MainHeaderName;
